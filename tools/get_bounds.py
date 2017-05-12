67e340a1efea919ca05863cced9a5ae691434356
Kumar Rohit Malhotra <krohitm@gmail.com>
2017-05-12 18:39:58 -0400

    backup to check median filtering

--- tools/get_bounds.py
+++ tools/get_bounds.py
@@ -4,13 +4,12 @@ from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
 from fast_rcnn.nms_wrapper import nms
 from utils.timer import Timer
-import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+#import matplotlib.pyplot as plt
+#plt.switch_backend('agg')
 import numpy as np
 import os, sys, cv2, csv
 import argparse
 from networks.factory import get_network
-import pylab
 
 
 CLASSES = ('__background__',
@@ -20,45 +19,8 @@ CLASSES = ('__background__',
            'motorbike', 'person', 'pottedplant',
            'sheep', 'sofa', 'train', 'tvmonitor')
 
-#has_class = 0
-
-#CLASSES = ('__background__','person','bike','motorbike','car','bus')
-
-def vis_detections(im, image_name, class_name, dets,ax, images_dir, thresh=0.5):
-    """Draw detected bounding boxes."""
-    #print "in vis_detections"
-    inds = np.where(dets[:, -1] >= thresh)[0]
-    if len(inds) == 0:
-        #print 'True'
-        return
-
-    class_flag = 0
-    for i in inds:
-        bbox = dets[i, :4]
-        if class_name=='person':
-            x_min, y_min, x_max, y_max = bbox[0], bbox[1], bbox[2], bbox[3]
-            bounds = [image_name, x_min, y_min, x_max, y_max] 
-            with open (os.path.join('/data0/krohitm/', 
-                                    'object_boundaries/person.csv'),'a') as f:
-                writer = csv.writer(f, delimiter = ',')
-                writer.writerow(bounds)
-        
-            score = dets[i, -1]
-
-            ax.add_patch(
-                plt.Rectangle((bbox[0], bbox[1]),
-                    bbox[2] - bbox[0],
-                    bbox[3] - bbox[1], fill=False,
-                    edgecolor='red', linewidth=3.5))
-            ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
-
-            plt.axis('off')
-            plt.tight_layout()
-            plt.draw()
-            pylab.savefig('/data0/krohitm/object_detection/{0}'.format(image_name), bbox_inches='tight')
+boundaries = []
+median_boundaries = []
 
 def demo(sess, net, image_dir, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
@@ -68,32 +30,41 @@ def demo(sess, net, image_dir, image_name):
     #im_file = os.path.join('/home/krohitm/code/Faster-RCNN_TF/data/temp_check/',image_name)
     im = cv2.imread(im_file)
     # Detect all object classes and regress object bounds
-    timer = Timer()
     timer.tic()
     scores, boxes = im_detect(sess, net, im)
-    timer.toc()
-    print ('Detection took {:.3f}s for '
-           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
-
-    # Visualize detections for each class
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
 
     CONF_THRESH = 0.8
     NMS_THRESH = 0.3
-    for cls_ind, cls in enumerate(CLASSES[1:]):
+    #person_found = 0
+    for cls_ind, cls in enumerate(CLASSES[15:16]):
         #print "in check"
-        cls_ind += 1 # because we skipped background
+        cls_ind += 15 # because we skipped background
         cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
         cls_scores = scores[:, cls_ind]
         dets = np.hstack((cls_boxes,
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
-	#print dets
-        vis_detections(im, image_name, cls, dets, ax, images_dir, thresh=CONF_THRESH)
-    plt.close('all')
+        #vis_detections(im, image_name, cls, dets, ax, images_dir, 
+        #               thresh=CONF_THRESH)
+        
+        inds = np.where(dets[:, -1] >= CONF_THRESH)[0]
+        if len(inds) == 0:
+            boundaries.append(np.array([os.path.join(image_dir, image_name
+                                                 ), -1, -1, -1, -1]))
+            return
+
+        #temp_boundaries = [0,0,0,0]
+        for i in inds:
+            bbox = dets[i, :4]
+            x_min, y_min, x_max, y_max = bbox[0], bbox[1], bbox[2], bbox[3]
+            boundaries.append(np.array(
+                        [os.path.join(image_dir, image_name), x_min, y_min, 
+                         x_max, y_max]))
+                
+    #if person_found == 0:
+    #    boundaries.append(np.array([os.path.join(image_dir, image_name
+    #                                             ), -1, -1, -1, -1]))
 
 def parse_args():
     """Parse input arguments."""
@@ -107,7 +78,8 @@ def parse_args():
                         default='VGGnet_test')
     parser.add_argument('--model', dest='model', help='Model path',
                         default=' ')
-    parser.add_argument('--images_dir', dest='images_dir', help='folder of images',
+    parser.add_argument('--images_dir', dest='images_dir', 
+                        help='parent folder of images',
                         default='None')
 
     args = parser.parse_args()
@@ -134,25 +106,62 @@ if __name__ == '__main__':
     #sess.run(tf.initialize_all_variables())
 
     print '\n\nLoaded network {:s}'.format(args.model)
-
-    # Warmup on a dummy image
-    im = 128 * np.ones((300, 300, 3), dtype=np.uint8)
-    for i in xrange(2):
-        _, _= im_detect(sess, net, im)
-
-    #im_names = ['000456.jpg', '000542.jpg', '001150.jpg',
-    #            '001763.jpg', '004545.jpg']
-    #im_names = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg']
-
-    #num_imgs = os.walk('ls -l {0} | wc -l'.format(images_dir))*2
-    #num_imgs = os.system('ls -l $images_dir| wc -l') - 1
-    _,_,files = os.walk("/data0/krohitm/posture_dataset/100GOPRO/frames/train_val/").next()
-    num_imgs = len(files)
-    #print num_imgs
-    with open (os.path.join('/data0/krohitm/','object_boundaries/person.csv'), 'w') as f:
-                f.write('x_min,y_min,x_max,y_max\n')
-    for i in range(1,num_imgs+1):
-        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-        print 'Getting bounds for out-{0}.jpg'.format((str(i)).zfill(7))
-        demo(sess, net, images_dir, 'out-{0}.jpg'.format((str(i)).zfill(7)))
-    #plt.show()
+    
+    dirpaths,dirnames,_ = os.walk('/data0/krohitm/posture_dataset/100GOPRO/frames/train_val'
+                                  ).next()
+    dirnames.sort()
+    #print "Following directories exist: ", dirnames
+    image_counter = 1
+    for dirpath, directory in zip(dirpaths, dirnames):
+        image_dir = os.path.join('/data0/krohitm/posture_dataset/100GOPRO/frames/train_val'
+                                 ,directory)
+        _,_,files = os.walk(image_dir).next()
+        num_imgs = len(files)
+        print image_dir
+        print type(files)
+        for i in range(1,num_imgs+1):
+            timer = Timer()
+            print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+            print 'Getting bounds for {0}/{1}.jpg'.format(directory, (
+                    str(i)).zfill(7))
+            timer.tic()
+            
+            demo(sess, net, image_dir, '{0}.jpg'.format((str(i)).zfill(7)))
+            
+            timer.toc()
+            print ("detection took {:.3f}s".format(timer.total_time))
+            image_counter+=1
+	    #remove next 3 lines
+          #  if image_counter >=50:
+         #   	break
+	#num_imgs = image_counter
+    
+    #take boundaries of 21 consecutive images in respective directories and find median bounds
+    surrounding_frames = 10
+    for dirpath, directory in zip(dirpaths, dirnames):
+        _,_,files = os.walk(os.path.join('data0/krohitm/posture_dataset/100GOPRO/frames/train_val',directory)).next()
+        num_imgs = len(files)
+        image_dir = os.path.join(dirpath, directory)
+        previous_set = 0
+        for i in range(1,num_imgs+1):
+            start = max(previous_set, previous_set+i- surrounding_frames)
+            end = min(previous_set+num_imgs,i+surrounding_frames)+1
+            bounds_for_median = np.median(boundaries[start:end, 1:5], axis = 0)
+            image_name_full = os.path.join(directory, 
+                                           '{0}.jpg'.format((str(i)).zfill(7)))
+            np.insert(bounds_for_median,0,image_name_full, axis=1)
+            median_boundaries.append(bounds_for_median)
+        previous_set = i
+    
+    with open (os.path.join('/data0/krohitm/','object_boundaries/person_median.csv'
+                            ), 'w') as f:
+        f.write('image_name, x_min,y_min,x_max,y_max\n')
+        writer = csv.writer(f, delimiter = ',')
+        writer.writerows(median_boundaries)
+    f.close()
+    
+    with open (os.path.join('/data0/krohitm/','object_boundaries/person.csv'
+                            ), 'w') as f:
+        f.write('image_name, x_min,y_min,x_max,y_max\n')
+        writer = csv.writer(f, delimiter = ',')
+        writer.writerows(boundaries)
